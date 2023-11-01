5af34397f0111d6752f78efb1dfd7e1ea6fff8fd
Cristina Lizbeth Martínez de la Cruz <itsirc_09@hotmail.com>
2023-10-31 18:37:19 -0600

    terminado

---
+++ react-to-do/src/organisms/tasks-group.jsx
@@ -0,0 +1,6 @@
+import Task from "../molecules/task";
+
+function TasksGroup({taskList, onDeleteTask}) {
+    return (taskList.map((task, idx) => <Task key={`task-${idx}`} taskText={task} onDeleteTask={onDeleteTask}/>));
+  }
+  export default TasksGroup;   
